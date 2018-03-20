608643717799c74d14b96620fec7471879198752
xhyrzldf <296796683@qq.com>
2018-03-20 22:20:25 +0800

    Initial commit

---
+++ src/main/kotlin/com/matrix/Hello.kt
@@ -0,0 +1,6 @@
+package com.matrix
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/matrix/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.matrix
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
